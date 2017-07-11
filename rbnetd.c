099ad5e9afaf497f1025e15e6d2abf6017a8c895
Neil Wenger <ncwenger@gmail.com>
2017-07-10 21:07:35 -0400

    started attempting to structure things. i can get a list of networks but I do so poorly

--- rbnetd.c
+++ rbnetd.c
@@ -10,23 +10,28 @@
 //#include <sys/ioctl.h>
 //#include <net/if.h>
 
-//https://stackoverflow.com/questions/5858655/linux-programmatically-up-down-an-interface-kernel
-//bring interface up
 
 //TODO next steps
-//confirm I can get a list of network names
-//confirm I can connect to a network
-//connect to a network using WPA2
+//get the right interface
+//bring interface up
+//  https://stackoverflow.com/questions/5858655/linux-programmatically-up-down-an-interface-kernel
+//I can get a list of network names
+//detect if the network needs a key
+//prompt for a key
+//confirm I can connect to a network using key
 //either use DHCP or manually acquire an IP address
+//config file
 //write function to connect via ethernet
 
 //add flags for:
 //IPv4 vs IPv6
 //specify an interface to use
 
+//commands:
+//conn/disc [ESSID]
+//list
 
-//XXX this only gets networks i'm connected to
-//...not really what i want at all
+
 int scan_intf(int sockd, char *intf, wireless_scan_head *head)
 {
     iwrange range;
@@ -44,53 +49,64 @@ int scan_intf(int sockd, char *intf, wireless_scan_head *head)
     return 0;
 }
 
+//FIXME this function sucks
+int get_net(int sockd, struct ifaddrs *intfs, wireless_scan_head *scan_head, wireless_scan* result, char *str)
+{
+    int search = strcmp("",str);
+    printf("Scanning interface %s\n", intfs->ifa_name);
+    int rv = scan_intf(sockd, intfs->ifa_name, scan_head);
+    if (rv) {
+        printf("Scan of interface %s failed\n", intfs->ifa_name);
+        return rv;
+    }
+    result = scan_head->result;
+
+    while(result != NULL)
+    {
+        if(!search)
+            printf("%s\n", result->b.essid);
+        else if(!strcmp(str,result->b.essid)) //need to set scan_result
+            return 0;
+        result = result->next;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     int sockd;
-    int inet_type = AF_INET; //IPv4, not 6
+    //int inet_type = AF_INET; //IPv4, not 6
     struct ifaddrs *intfs;
     wireless_scan_head *scan_head = (wireless_scan_head*)malloc(sizeof(wireless_scan_head*));
     wireless_scan *scan_result;
+
+    //check that interfaces are up
+    sockd = iw_sockets_open();
 
     if (getifaddrs(&intfs) < 0) {
         printf("FAILED TO GET INTERFACE LIST\n");
         return 1;
     }
-    while (intfs->ifa_next != NULL)
-    {
-        if (!(intfs->ifa_flags & IFF_LOOPBACK) &&
-           intfs->ifa_addr->sa_family == inet_type) {
+
+    while (intfs->ifa_next != NULL) {
+        if(!strcmp("wlp1s0",intfs->ifa_name)) //FIXME hardcoded
             break;
-        }
         intfs = intfs->ifa_next;
     }
-
     //TODO safety checks to ensure current intfs node is what we want to use
-    // exit otherwise
 
-    sockd = iw_sockets_open();
-
-    printf("Scanning interface %s\n", intfs->ifa_name);
-    int rv = scan_intf(sockd, intfs->ifa_name, scan_head);
-    if (rv) {
-        printf("Scan of interface %s failed\n", intfs->ifa_name);
-        exit(rv);
-    }
-    scan_result = scan_head->result;
-
-    while(scan_result != NULL)
+    if(get_net(sockd,intfs,scan_head,scan_result,"Leps")) //FIXME hardcoded
     {
-        printf("%s\n", scan_result->b.essid);
-        scan_result = scan_result->next;
+        printf("FAILED TO LIST NETWORKS\n");
+        return 2;
     }
-    //freeifaddrs(intfs); //segfault
-    /*
+
     if (scan_result->has_ap_addr) {
-        if (connect(sockfd, scan_result->ap_addr, sizeof(scan_result->ap_addr)) < 0) {
+        if (connect(sockd, (const struct sockaddr*)&scan_result->ap_addr, sizeof(scan_result->ap_addr)) < 0) {
             printf("Connect to network:%s failed\n", scan_result->b.essid);
-            exit(3);
+            return 3;
         }
     }
-    */
+
     return 0;
 }
